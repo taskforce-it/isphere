dd9eeec235b4c371a0ff5db8e0fedcec4ce5f03f
Thomas Raddatz <thomas.raddatz@tools400.de>
2014-03-10 21:42:37 +0000

    Renamed remaining references to "de.taskforce" to "biz".

---
+++ branches/Transform Spooled File (experimential)/rse1/iSphereRSEPlugin/src/biz/isphere/rse/internal/MessageFileSearchObjectFilterCreator.java
@@ -0,0 +1,35 @@
+package biz.isphere.rse.internal;
+
+import com.ibm.etools.iseries.comm.filters.ISeriesObjectFilterString;
+import com.ibm.etools.iseries.core.api.ISeriesConnection;
+
+import biz.isphere.internal.IMessageFileSearchObjectFilterCreator;
+import biz.isphere.messagefilesearch.SearchResult;
+
+public class MessageFileSearchObjectFilterCreator implements IMessageFileSearchObjectFilterCreator {
+
+	public boolean createObjectFilter(Object connection, String filterName, SearchResult[] searchResults) {
+		
+		ISeriesObjectFilterString[] filterStrings = new ISeriesObjectFilterString[searchResults.length];
+
+		for (int idx = 0; idx < searchResults.length; idx++) {
+			
+			ISeriesObjectFilterString filterString = new ISeriesObjectFilterString();
+			filterString.setLibrary(searchResults[idx].getLibrary());
+			filterString.setObject(searchResults[idx].getMessageFile());
+			filterString.setObjectType("*MSGF");
+
+			filterStrings[idx] = filterString;
+			
+		}
+		
+		if (RSEHelper.createObjectFilter((ISeriesConnection)connection, filterName, filterStrings) == null) {
+			return false;
+		}
+		else {
+			return true;
+		}
+		
+	}
+
+}
