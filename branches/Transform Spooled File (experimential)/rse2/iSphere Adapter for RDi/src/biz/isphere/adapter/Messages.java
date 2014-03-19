ba117545c596220a536e231ffcc69eda825b205d
Thomas Raddatz <thomas.raddatz@tools400.de>
2014-03-19 12:39:26 +0000

    Fixed invalid bundle name and added JavaDoc and SWTException.

--- branches/Transform Spooled File (experimential)/rse2/iSphere Adapter for RDi/src/biz/isphere/adapter/Messages.java
+++ branches/Transform Spooled File (experimential)/rse2/iSphere Adapter for RDi/src/biz/isphere/adapter/Messages.java
@@ -4,7 +4,7 @@ import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
 
-    private static final String BUNDLE_NAME = "biz.isphere.adapter"; //$NON-NLS-1$
+    private static final String BUNDLE_NAME = "biz.isphere.adapter.messages"; //$NON-NLS-1$
 
     static {
         // initialize resource bundle
