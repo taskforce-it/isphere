4ade51e5d27c05c7fe00e333c0b7fb51a536f3ba
tools400 <thomas.raddatz@tools400.de>
2017-08-04 13:05:44 +0000

    Renaming 'contentProviders' to 'contentproviders'.

--- branches/Journal Explorer (Beta)/iSphere Journal Explorer Core Plugin/src/biz/isphere/journalexplorer/core/ui/contentproviders2/JournalViewerContentProvider.java
+++ branches/Journal Explorer (Beta)/iSphere Journal Explorer Core Plugin/src/biz/isphere/journalexplorer/core/ui/contentproviders2/JournalViewerContentProvider.java
@@ -9,7 +9,7 @@
  * Continued and adopted to iSphere: iSphere Project Team
  *******************************************************************************/
 
-package biz.isphere.journalexplorer.core.ui.contentProviders;
+package biz.isphere.journalexplorer.core.ui.contentproviders2;
 
 import java.util.ArrayList;
 
