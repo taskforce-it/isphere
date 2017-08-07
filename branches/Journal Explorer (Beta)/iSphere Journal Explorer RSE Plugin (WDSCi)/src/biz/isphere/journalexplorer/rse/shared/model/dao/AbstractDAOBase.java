25ddeb67afc04d85b679eac4f90aa968659a61db
tools400 <thomas.raddatz@tools400.de>
2017-08-07 09:34:47 +0000

    Changes for moving the journal explorer into the iSphere project.

--- branches/Journal Explorer (Beta)/iSphere Journal Explorer RSE Plugin (WDSCi)/src/biz/isphere/journalexplorer/rse/shared/model/dao/AbstractDAOBase.java
+++ branches/Journal Explorer (Beta)/iSphere Journal Explorer RSE Plugin (WDSCi)/src/biz/isphere/journalexplorer/rse/shared/model/dao/AbstractDAOBase.java
@@ -12,23 +12,21 @@
 package biz.isphere.journalexplorer.rse.shared.model.dao;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 import biz.isphere.journalexplorer.rse.Messages;
 import biz.isphere.journalexplorer.rse.shared.as400fields.AS400Date;
 
 import com.ibm.etools.iseries.core.api.ISeriesConnection;
 
-public class DAOBase {
+public abstract class AbstractDAOBase {
+    protected static final String properties = "thread used=false;extendeddynamic=true;package criteria=select;package cache=true;"; //$NON-NLS-1$
 
     protected ISeriesConnection ibmiConnection;
     private Connection connection;
     private String dateFormat;
     private String dateSeparator;
 
-    public DAOBase(String connectionName) throws Exception {
+    public AbstractDAOBase(String connectionName) throws Exception {
         if (connectionName != null) {
             this.ibmiConnection = ISeriesConnection.getConnection(connectionName);
             if (!ibmiConnection.isConnected()) {
@@ -51,39 +49,15 @@ public class DAOBase {
     public void destroy() {
     }
 
+    protected int getDateFormat() {
+        return AS400Date.toFormat(dateFormat);
+    }
+
+    protected Connection getConnection() {
+        return connection;
+    }
+
     protected String getConnectionName() {
         return ibmiConnection.getConnectionName();
-    }
-
-    protected PreparedStatement prepareStatement(String sql) throws SQLException {
-        return this.connection.prepareStatement(sql);
-    }
-
-    protected int getDateFormat() {
-        return AS400Date.toFormat(this.dateFormat);
-    }
-
-    protected void destroy(Connection connection) throws Exception {
-        if (connection != null && !connection.isClosed()) connection.close();
-    }
-
-    protected void destroy(ResultSet resultSet) throws Exception {
-        if (resultSet != null) resultSet.close();
-    }
-
-    protected void destroy(PreparedStatement preparedStatement) throws Exception {
-        if (preparedStatement != null) preparedStatement.close();
-    }
-
-    protected void rollback(Connection connection) throws Exception {
-        if (connection != null && !connection.isClosed()) {
-            if (connection.getAutoCommit() == false) connection.rollback();
-        }
-    }
-
-    protected void commit(Connection connection) throws Exception {
-        if (connection != null && !connection.isClosed()) {
-            if (connection.getAutoCommit() == false) connection.commit();
-        }
     }
 }
