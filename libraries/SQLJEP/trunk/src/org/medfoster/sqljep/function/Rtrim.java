7198509d652d0146c1e752153fda2f6e3d399d69
tools400 <thomas.raddatz@tools400.de>
2018-12-20 14:45:09 +0000

    Formatted source code and suppressed warnings.

--- libraries/SQLJEP/trunk/src/org/medfoster/sqljep/function/Rtrim.java
+++ libraries/SQLJEP/trunk/src/org/medfoster/sqljep/function/Rtrim.java
@@ -8,49 +8,54 @@
            (c) Copyright 2002, Nathan Funk
  
       See LICENSE.txt for license information.
-*****************************************************************************/
+ *****************************************************************************/
 
 package org.medfoster.sqljep.function;
 
-import org.medfoster.sqljep.*;
+import org.medfoster.sqljep.ASTFunNode;
+import org.medfoster.sqljep.JepRuntime;
+import org.medfoster.sqljep.ParseException;
 
 public class Rtrim extends PostfixCommand {
-	final public int getNumberOfParameters() {
-		return -1;
-	}
-	
-	public void evaluate(ASTFunNode node, JepRuntime runtime) throws ParseException {
-		node.childrenAccept(runtime.ev, null);
-		int num = node.jjtGetNumChildren();
-		if (num == 1) {
-			Comparable param1 = runtime.stack.pop();
-			runtime.stack.push(rtrim(param1, " "));
-		}
-		else if (num == 2) {
-			Comparable param2 = runtime.stack.pop();
-			Comparable param1 = runtime.stack.pop();
-			runtime.stack.push(rtrim(param1, param2));
-		} else {
-			// remove all parameters from stack and push null
-			removeParams(runtime.stack, num);
-			throw new ParseException(PARAMS_NUMBER+" for rtrim");
-		}
-	}
-	
-	public static String rtrim(Comparable param1, Comparable param2) throws ParseException {
-		if (param1 == null || param2 == null) {
-			return null;
-		}
-		String inputStr = param1.toString();
-		String chars = param2.toString();
-		int i;
-		for (i = inputStr.length()-1; i >= 0; i--) {
-			char c = inputStr.charAt(i);
-			if (chars.indexOf(c) == -1) {
-				break;
-			}
-		}
-		return inputStr.substring(0, i+1);
-	}
-}
 
+    final public int getNumberOfParameters() {
+        return -1;
+    }
+
+    public void evaluate(ASTFunNode node, JepRuntime runtime) throws ParseException {
+        node.childrenAccept(runtime.ev, null);
+        int num = node.jjtGetNumChildren();
+        if (num == 1) {
+            Comparable<?> param1 = runtime.stack.pop();
+            runtime.stack.push(rtrim(param1, " "));
+        } else if (num == 2) {
+            Comparable<?> param2 = runtime.stack.pop();
+            Comparable<?> param1 = runtime.stack.pop();
+            runtime.stack.push(rtrim(param1, param2));
+        } else {
+            // remove all parameters from stack and push null
+            removeParams(runtime.stack, num);
+            throw new ParseException(PARAMS_NUMBER + " for rtrim");
+        }
+    }
+
+    public static String rtrim(Comparable<?> param1, Comparable<?> param2) throws ParseException {
+
+        if (param1 == null || param2 == null) {
+            return null;
+        }
+
+        String inputStr = param1.toString();
+        String chars = param2.toString();
+
+        int i;
+        for (i = inputStr.length() - 1; i >= 0; i--) {
+            char c = inputStr.charAt(i);
+            if (chars.indexOf(c) == -1) {
+                break;
+            }
+        }
+
+        return inputStr.substring(0, i + 1);
+    }
+}
