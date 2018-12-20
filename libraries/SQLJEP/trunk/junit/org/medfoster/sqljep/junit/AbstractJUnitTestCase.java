6a0eeee0cebf107cb8a276dd393490b7020078ef
tools400 <thomas.raddatz@tools400.de>
2018-12-20 14:39:49 +0000

    Added missing copyright notice.

--- libraries/SQLJEP/trunk/junit/org/medfoster/sqljep/junit/AbstractJUnitTestCase.java
+++ libraries/SQLJEP/trunk/junit/org/medfoster/sqljep/junit/AbstractJUnitTestCase.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) project_year-2018 project_team
+ * Copyright (c) 2012-2018 iSphere Project Owners
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
