ffc685a3e7bc5c97c7cac6ec1e0280e32bc8af1c
tools400 <thomas.raddatz@tools400.de>
2018-12-20 14:43:44 +0000

    Formatted source code and suppressed warnings.

--- libraries/SQLJEP/trunk/src/org/medfoster/sqljep/function/Trim.java
+++ libraries/SQLJEP/trunk/src/org/medfoster/sqljep/function/Trim.java
@@ -8,27 +8,29 @@
            (c) Copyright 2002, Nathan Funk
  
       See LICENSE.txt for license information.
-*****************************************************************************/
+ *****************************************************************************/
 package org.medfoster.sqljep.function;
 
-import org.medfoster.sqljep.*;
+import org.medfoster.sqljep.ASTFunNode;
+import org.medfoster.sqljep.JepRuntime;
+import org.medfoster.sqljep.ParseException;
 
 public class Trim extends PostfixCommand {
-	final public int getNumberOfParameters() {
-		return 1;
-	}
-	
-	public void evaluate(ASTFunNode node, JepRuntime runtime) throws ParseException {
-		node.childrenAccept(runtime.ev, null);
-		Comparable param = runtime.stack.pop();
-		runtime.stack.push(trim(param));		//push the result on the inStack
-	}
 
-	public static String trim(Comparable param) throws ParseException {
-		if (param == null) {
-			return null;
-		}
-		return param.toString().trim();
-	}
+    final public int getNumberOfParameters() {
+        return 1;
+    }
+
+    public void evaluate(ASTFunNode node, JepRuntime runtime) throws ParseException {
+        node.childrenAccept(runtime.ev, null);
+        Comparable<?> param = runtime.stack.pop();
+        runtime.stack.push(trim(param)); // push the result on the inStack
+    }
+
+    public static String trim(Comparable<?> param) throws ParseException {
+        if (param == null) {
+            return null;
+        }
+        return param.toString().trim();
+    }
 }
-
