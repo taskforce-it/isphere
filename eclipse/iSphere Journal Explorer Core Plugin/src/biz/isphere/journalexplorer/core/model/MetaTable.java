4198816e575c061894876fb84b7bc6c61e8522db
tools400 <thomas.raddatz@tools400.de>
2021-03-06 17:08:20 +0000

    Removed obsolete property.

--- eclipse/iSphere Journal Explorer Core Plugin/src/biz/isphere/journalexplorer/core/model/MetaTable.java
+++ eclipse/iSphere Journal Explorer Core Plugin/src/biz/isphere/journalexplorer/core/model/MetaTable.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012-2020 iSphere Project Owners
+ * Copyright (c) 2012-2021 iSphere Project Owners
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
@@ -49,7 +49,6 @@ public class MetaTable {
 
     private boolean loaded;
     private int parsingOffset;
-    private boolean isJournalOutputFile;
     private JournalOutputType outfileType;
     private int countNullableFields;
     private int lastNullableFieldIndex;
