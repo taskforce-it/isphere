95397d5bbc6a91074ea80cb9e42d684b3da5f7c3
tools400 <thomas.raddatz@tools400.de>
2021-04-09 16:25:21 +0000

    Changed the iSphere menu "biz.isphere.core.menu.isphere" to be defined as a "menuContribution".

--- trunk/eclipse/iSphere Core Plugin/src/biz/isphere/core/connection/rse/ISphereConnectionPropertyPageDelegate.java
+++ trunk/eclipse/iSphere Core Plugin/src/biz/isphere/core/connection/rse/ISphereConnectionPropertyPageDelegate.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012-2020 iSphere Project Owners
+ * Copyright (c) 2012-2021 iSphere Project Owners
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
@@ -26,7 +26,11 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.PlatformUI;
+
+import com.ibm.as400.access.AS400;
 
 import biz.isphere.base.internal.IntHelper;
 import biz.isphere.base.internal.StringHelper;
@@ -39,8 +43,6 @@ import biz.isphere.core.internal.Validator;
 import biz.isphere.core.internal.handler.TransferLibraryHandler;
 import biz.isphere.core.preferences.Preferences;
 import biz.isphere.core.swt.widgets.WidgetFactory;
-
-import com.ibm.as400.access.AS400;
 
 public class ISphereConnectionPropertyPageDelegate {
 
@@ -280,15 +282,12 @@ public class ISphereConnectionPropertyPageDelegate {
             public void widgetSelected(SelectionEvent event) {
                 if (listenersEnabled) {
                     if (validateAll()) {
-                        String hostName = textConnectionName.getText();
+
+                        Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+                        String connectionName = textConnectionName.getText();
                         int ftpPort = IntHelper.tryParseInt(textFtpPortNumber.getText(), Preferences.getInstance().getDefaultFtpPortNumber());
-                        TransferLibraryHandler handler = new TransferLibraryHandler(hostName, ftpPort, iSphereLibrary, aspGroup);
-                        handler.setConnectionsEnabled(false);
-                        try {
-                            handler.execute(null);
-                        } catch (Throwable e) {
-                            ISpherePlugin.logError("*** Failed to transfer iSphere library ***", e); //$NON-NLS-1$
-                        }
+                        TransferLibraryHandler handler = new TransferLibraryHandler();
+                        handler.execute(shell, connectionName, ftpPort, iSphereLibrary, aspGroup, false);
                     }
                 }
             }
--- trunk/eclipse/iSphere Core Plugin/src/biz/isphere/core/internal/handler/TransferLibraryHandler.java
+++ trunk/eclipse/iSphere Core Plugin/src/biz/isphere/core/internal/handler/TransferLibraryHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012-2020 iSphere Project Owners
+ * Copyright (c) 2012-2021 iSphere Project Owners
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,45 +8,27 @@
 
 package biz.isphere.core.internal.handler;
 
-import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.commands.IHandler;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.PlatformUI;
 
 import biz.isphere.base.internal.StringHelper;
-import biz.isphere.core.ISpherePlugin;
 import biz.isphere.core.Messages;
 import biz.isphere.core.internal.TransferISphereLibrary;
+import biz.isphere.core.preferences.Preferences;
 
 /**
  * This class is the action handler of the "TransferLibraryAction".
  */
-public class TransferLibraryHandler extends AbstractHandler implements IHandler {
-
-    private String connectionName;
-    private int ftpPort;
-    private String iSphereLibrary;
-    private String aspGroup;
-    private boolean connectionsEnabled;
+public class TransferLibraryHandler extends AbstractShellHandler {
 
     /**
      * Default constructor, used by the Eclipse framework.
      */
     public TransferLibraryHandler() {
         super();
-    }
-
-    public TransferLibraryHandler(String connectionName, int ftpPort, String iSphereLibrary, String aspGroup) {
-        this.connectionName = connectionName;
-        this.ftpPort = ftpPort;
-        this.iSphereLibrary = iSphereLibrary;
-        this.aspGroup = aspGroup;
-
-        setConnectionsEnabled(true);
     }
 
     /*
@@ -57,28 +39,41 @@ public class TransferLibraryHandler extends AbstractHandler implements IHandler 
      */
     public Object execute(ExecutionEvent event) throws ExecutionException {
 
-        try {
+        String iSphereLibrary = Preferences.getInstance().getISphereLibrary(); // CHECKED
 
-            Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
-
-            if (StringHelper.isNullOrEmpty(iSphereLibrary)) {
-                MessageDialog.openError(shell, Messages.Error, Messages.iSphere_library_not_set_in_preferences);
-                return null;
-            }
-
-            TransferISphereLibrary statusDialog = new TransferISphereLibrary(shell, SWT.APPLICATION_MODAL | SWT.SHELL_TRIM, iSphereLibrary, aspGroup,
-                connectionName, ftpPort);
-
-            statusDialog.setConnectionsEnabled(connectionsEnabled);
-            statusDialog.open();
-
-        } catch (Throwable e) {
-            ISpherePlugin.logError("*** Failed to invoke the 'Transfer Library' handler ***", e); //$NON-NLS-1$
+        if (StringHelper.isNullOrEmpty(iSphereLibrary)) {
+            MessageDialog.openError(getShell(event), Messages.Error, Messages.iSphere_library_not_set_in_preferences);
+            return null;
         }
-        return null;
+
+        String aspGroup = Preferences.getInstance().getASPGroup();
+        String connectionName = Preferences.getInstance().getConnectionName();
+        int ftpPort = Preferences.getInstance().getFtpPortNumber();
+
+        return execute(getShell(event), connectionName, ftpPort, iSphereLibrary, aspGroup, true);
     }
 
-    public void setConnectionsEnabled(boolean enabled) {
-        this.connectionsEnabled = enabled;
+    /**
+     * Called from various iSphere classes, such as <i>preferences page</i>,
+     * <i>transfer library</i> menu item and <i>RSE properties page</i>.
+     * 
+     * @param shell - the parent shell.
+     * @param connectionName - the connection name.
+     * @param ftpPort - the ftp port number.
+     * @param iSphereLibrary - the name of the iSphere library.
+     * @param aspGroup - the asp group the library is put into.
+     * @param selectConnectionEnabled - whether the user can change the
+     *        connection name.
+     * @return null
+     */
+    public Object execute(Shell shell, String connectionName, int ftpPort, String iSphereLibrary, String aspGroup, boolean selectConnectionEnabled) {
+
+        TransferISphereLibrary statusDialog = new TransferISphereLibrary(shell, SWT.APPLICATION_MODAL | SWT.SHELL_TRIM, iSphereLibrary, aspGroup,
+            connectionName, ftpPort);
+
+        statusDialog.setConnectionsEnabled(selectConnectionEnabled);
+        statusDialog.open();
+
+        return null;
     }
 }
