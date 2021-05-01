9f26fb87a5d24f2552ec30c45d837cf996566945
tools400 <thomas.raddatz@tools400.de>
2021-05-01 14:20:51 +0000

    Added/updated Javadoc.

--- trunk/rse2 (RDi)/iSphere RSE Plugin (RDi)/src/biz/isphere/rse/ibmi/contributions/extension/point/QualifiedConnectionName.java
+++ trunk/rse2 (RDi)/iSphere RSE Plugin (RDi)/src/biz/isphere/rse/ibmi/contributions/extension/point/QualifiedConnectionName.java
@@ -14,6 +14,16 @@ import org.eclipse.rse.internal.core.model.SystemProfileManager;
 
 import com.ibm.etools.iseries.subsystems.qsys.api.IBMiConnection;
 
+/**
+ * This class represents a remote system connection name. A connection name
+ * consists of two parts:
+ * <ul>
+ * <li>profile name</li>
+ * <li>connection name</li>
+ * </ul>
+ * The format of a qualified connection name is
+ * <code>'profileName:connectionName'</code>.
+ */
 public class QualifiedConnectionName implements Comparable<QualifiedConnectionName> {
 
     private static final String CONNECTION_NAME_DELIMITER = ":"; //$NON-NLS-1$
@@ -24,21 +34,50 @@ public class QualifiedConnectionName implements Comparable<QualifiedConnectionNa
     private String connectionName;
     private String profileName;
 
+    /**
+     * Constructs a qualified connection name object from a given remote system
+     * host.
+     * 
+     * @param host - A remote system host.
+     */
     public QualifiedConnectionName(IHost host) {
         this.profileName = host.getSystemProfileName();
         this.connectionName = host.getAliasName();
     }
 
+    /**
+     * Constructs a qualified connection name object from a given remote system
+     * connection.
+     * 
+     * @param host - A remote system connection.
+     */
     public QualifiedConnectionName(IBMiConnection connection) {
         this.profileName = connection.getProfileName();
         this.connectionName = connection.getConnectionName();
     }
 
+    /**
+     * Constructs a qualified connection name object from given profile and
+     * connection names.
+     * 
+     * @param profileName - A remote system profile name.
+     * @param connectionName - A remote system connection name.
+     */
     public QualifiedConnectionName(String profileName, String connectionName) {
         this.profileName = profileName;
         this.connectionName = connectionName;
     }
 
+    /**
+     * Constructs a qualified connection name object from a qualified connection
+     * name string, which must be formatted as
+     * <code>'profileName:connectionName'</code>. The profile name is optional.<br>
+     * When the profile name is missing, the constructor falls back to the
+     * default system profile name.
+     * 
+     * @param qualifiedConnectionName - A qualified connection name of format
+     *        <code>'profileName:connectionName'</code>.
+     */
     public QualifiedConnectionName(String qualifiedConnectionName) {
 
         String[] parts = qualifiedConnectionName.split(CONNECTION_NAME_DELIMITER);
