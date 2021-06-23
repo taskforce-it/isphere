ce2c3871abf53b418a0e6e51f0831347489d6f9b
f_hildebrandt <fhildebrandt@taskforce-it.de>
2021-06-23 15:08:24 +0000

    Removed contructors and methods no longer used by CMOne.

--- eclipse/iSphere Core Plugin/src/biz/isphere/core/userspaceeditor/UserSpaceEditor.java
+++ eclipse/iSphere Core Plugin/src/biz/isphere/core/userspaceeditor/UserSpaceEditor.java
@@ -11,16 +11,12 @@ package biz.isphere.core.userspaceeditor;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 
-import biz.isphere.core.ISpherePlugin;
-import biz.isphere.core.annotations.CMOne;
 import biz.isphere.core.dataspace.WrappedDataSpace;
 import biz.isphere.core.dataspace.rse.AbstractWrappedDataSpace;
 import biz.isphere.core.dataspaceeditor.AbstractDataSpaceEditor;
 import biz.isphere.core.dataspaceeditor.UserSpaceEditorInput;
 import biz.isphere.core.internal.ISphereHelper;
 import biz.isphere.core.internal.RemoteObject;
-
-import com.ibm.as400.access.AS400;
 
 public class UserSpaceEditor extends AbstractDataSpaceEditor {
 
@@ -29,20 +25,6 @@ public class UserSpaceEditor extends AbstractDataSpaceEditor {
     @Override
     protected AbstractWrappedDataSpace createDataSpaceWrapper(RemoteObject remoteObject) throws Exception {
         return new WrappedDataSpace(remoteObject);
-    }
-
-    @CMOne(info = "This method is used by CMOne")
-    public static void openEditor(AS400 anAS400, RemoteObject remoteObject, String aMode) {
-        if (ISphereHelper.checkISphereLibrary(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), anAS400)) {
-            try {
-
-                UserSpaceEditorInput editorInput = new UserSpaceEditorInput(anAS400, remoteObject, aMode);
-                PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().openEditor(editorInput, UserSpaceEditor.ID);
-
-            } catch (PartInitException e) {
-                ISpherePlugin.logError("Failed to open user space editor", e); //$NON-NLS-1$
-            }
-        }
     }
 
     public static void openEditor(String connectionName, RemoteObject remoteObject, String aMode) throws PartInitException {
