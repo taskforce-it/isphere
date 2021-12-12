21fae5f3503e3fe86e2f48c6ecd16a73497fc1ef
tools400 <thomas.raddatz@tools400.de>
2021-12-12 16:25:51 +0000

    Added isSkipGapsEnabled property to renaming rule. Default=true.

--- eclipse/iSphere Core Plugin/src/biz/isphere/core/memberrename/RenameMemberActor.java
+++ eclipse/iSphere Core Plugin/src/biz/isphere/core/memberrename/RenameMemberActor.java
@@ -10,8 +10,15 @@ package biz.isphere.core.memberrename;
 
 import java.beans.PropertyVetoException;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+
+import biz.isphere.base.comparators.QSYSObjectPathNameComparator;
+import biz.isphere.core.Messages;
+import biz.isphere.core.memberrename.exceptions.InvalidMemberNameException;
+import biz.isphere.core.memberrename.exceptions.NoMoreNamesAvailableException;
+import biz.isphere.core.memberrename.rules.IMemberRenamingRule;
 
 import com.ibm.as400.access.AS400;
 import com.ibm.as400.access.AS400Exception;
@@ -21,11 +28,6 @@ import com.ibm.as400.access.MemberDescription;
 import com.ibm.as400.access.MemberList;
 import com.ibm.as400.access.ObjectDoesNotExistException;
 import com.ibm.as400.access.QSYSObjectPathName;
-
-import biz.isphere.core.Messages;
-import biz.isphere.core.memberrename.exceptions.InvalidMemberNameException;
-import biz.isphere.core.memberrename.exceptions.NoMoreNamesAvailableException;
-import biz.isphere.core.memberrename.rules.IMemberRenamingRule;
 
 public class RenameMemberActor {
 
@@ -45,13 +47,40 @@ public class RenameMemberActor {
         qsysMemberPaths.clear();
     }
 
+    /**
+     * Adds a member path name to the list of existing members. This method is
+     * only for JUnit testing without needing a system connection.
+     * 
+     * @param memberPath - path of the member that is added to the list of
+     *        existing members
+     * @throws PropertyVetoException
+     */
     public void addMemberName(QSYSObjectPathName memberPath) throws PropertyVetoException {
-        qsysMemberPaths.add(memberPath.getPath());
+        addMemberName(memberPath.getPath());
     }
 
-    public QSYSObjectPathName produceNewMemberName(QSYSObjectPathName oldMemberPath)
-        throws NoMoreNamesAvailableException, InvalidMemberNameException, PropertyVetoException, AS400Exception, AS400SecurityException,
-        ErrorCompletingRequestException, IOException, InterruptedException, ObjectDoesNotExistException {
+    public void addMemberName(String memberPath) throws PropertyVetoException {
+        qsysMemberPaths.add(memberPath);
+    }
+
+    /**
+     * Produces a new member name based of a given member and renaming rule.
+     * 
+     * @param oldMemberPath - name of member that is renamed
+     * @return QSYS path of the new member.
+     * @throws NoMoreNamesAvailableException
+     * @throws InvalidMemberNameException
+     * @throws PropertyVetoException
+     * @throws AS400Exception
+     * @throws AS400SecurityException
+     * @throws ErrorCompletingRequestException
+     * @throws IOException
+     * @throws InterruptedException
+     * @throws ObjectDoesNotExistException
+     */
+    public QSYSObjectPathName produceNewMemberName(QSYSObjectPathName oldMemberPath) throws NoMoreNamesAvailableException,
+        InvalidMemberNameException, PropertyVetoException, AS400Exception, AS400SecurityException, ErrorCompletingRequestException, IOException,
+        InterruptedException, ObjectDoesNotExistException {
 
         String oldMemberName = oldMemberPath.getMemberName();
         if (oldMemberName.length() > 9) {
@@ -60,7 +89,6 @@ public class RenameMemberActor {
             throw new InvalidMemberNameException(Messages.bind(Messages.Error_Invalid_member_name_Name_is_too_long_A, oldMemberName));
         }
 
-        MemberDescription[] memberDescriptions = null;
         if (system != null) {
 
             String baseMemberName = memberRenamingRule.getBaseName(oldMemberPath.getMemberName());
@@ -71,7 +99,22 @@ public class RenameMemberActor {
 
             MemberList memberList = new MemberList(system, qsysMemberFilter);
             memberList.load();
-            memberDescriptions = memberList.getMemberDescriptions();
+            MemberDescription[] memberDescriptions = memberList.getMemberDescriptions();
+
+            clearMemberList();
+            for (MemberDescription memberDescription : memberDescriptions) {
+                addMemberName(memberDescription.getPath());
+            }
+        }
+
+        if (memberRenamingRule.isSkipGapsEnabled() && qsysMemberPaths.size() > 0) {
+            String[] memberPaths = qsysMemberPaths.toArray(new String[qsysMemberPaths.size()]);
+            QSYSObjectPathName[] qsysMemberPaths = new QSYSObjectPathName[memberPaths.length];
+            for (int i = 0; i < memberPaths.length; i++) {
+                qsysMemberPaths[i] = new QSYSObjectPathName(memberPaths[i]);
+            }
+            Arrays.sort(qsysMemberPaths, new QSYSObjectPathNameComparator());
+            oldMemberPath = qsysMemberPaths[qsysMemberPaths.length - 1];
         }
 
         QSYSObjectPathName nextMemberNamePath = null;
@@ -84,26 +127,12 @@ public class RenameMemberActor {
 
             nextMemberNamePath = new QSYSObjectPathName(oldMemberPath.getPath());
             nextMemberNamePath.setMemberName(nextMemberName);
-            if (qsysMemberPaths.contains(nextMemberNamePath.getPath()) || isMemberOnSystem(memberDescriptions, nextMemberNamePath)) {
+            if (qsysMemberPaths.contains(nextMemberNamePath.getPath())) {
                 oldMemberPath = nextMemberNamePath;
                 nextMemberNamePath = null;
             }
         }
 
         return nextMemberNamePath;
-    }
-
-    private boolean isMemberOnSystem(MemberDescription[] memberDescriptions, QSYSObjectPathName newName) throws PropertyVetoException, AS400Exception,
-        AS400SecurityException, ErrorCompletingRequestException, IOException, InterruptedException, ObjectDoesNotExistException {
-
-        if (memberDescriptions != null) {
-            for (MemberDescription memberDescription : memberDescriptions) {
-                if (memberDescription.getValue(MemberDescription.MEMBER_NAME).equals(newName.getMemberName())) {
-                    return true;
-                }
-            }
-        }
-
-        return false;
     }
 }
