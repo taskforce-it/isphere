2607da66e622a81c7ee93c256b6d352a7f7343be
tools400 <thomas.raddatz@tools400.de>
2021-12-19 12:57:25 +0000

    Reworked renaming rule so that backup member names can be renamed, too.

--- eclipse/iSphere Core Plugin/src/biz/isphere/core/memberrename/rules/IMemberRenamingRule.java
+++ eclipse/iSphere Core Plugin/src/biz/isphere/core/memberrename/rules/IMemberRenamingRule.java
@@ -10,18 +10,68 @@ package biz.isphere.core.memberrename.rules;
 
 import java.beans.PropertyVetoException;
 
+import com.ibm.as400.access.QSYSObjectPathName;
+
+import biz.isphere.core.memberrename.RenameMemberActor;
 import biz.isphere.core.memberrename.adapters.IMemberRenamingRuleAdapter;
 import biz.isphere.core.memberrename.exceptions.NoMoreNamesAvailableException;
 
+/**
+ * This class specifies the rules that are applied when producing a new backup
+ * member name.
+ */
 public interface IMemberRenamingRule {
 
+    /**
+     * Returns the UI label of the rule.
+     * 
+     * @return UI label
+     */
     public String getLabel();
 
-    public boolean isSkipGapsEnabled();
+    /**
+     * Sets the name of the member that is renamed. This is the first method
+     * that must be called after a new rule has been created.
+     * 
+     * @param memberName - name of the member that is renamed.
+     */
+    public void setBaseMemberName(String memberName);
 
-    public String getBaseName(String memberName) throws NoMoreNamesAvailableException, PropertyVetoException;
+    /**
+     * Returns the mask for producing a list of members that exist on the
+     * system. This method is called by the {@link RenameMemberActor} in order
+     * to produce a list of members that exist on the system.
+     * 
+     * @return list of existing members
+     */
+    public String getMemberNameFilter();
 
-    public String getNextName(String currentMemberName) throws NoMoreNamesAvailableException, PropertyVetoException;
+    /**
+     * Optional method. Sets the list of members that exist on the system. This
+     * method can be called before <code>getNextName(String)</code>. It can be
+     * used for calculating the last (highest) backup member name used, so that
+     * gaps can be skipped.
+     * 
+     * @param existingMemberPaths - list of members on the system, that matches
+     *        the rule. The path must match the value returned by
+     *        {@link QSYSObjectPathName#getPath()}.
+     */
+    public void setExistingMembers(String[] existingMemberPaths);
 
+    /**
+     * Returns the next backup member name.
+     * 
+     * @return next backup member name
+     * @throws NoMoreNamesAvailableException
+     * @throws PropertyVetoException
+     */
+    public String getNextName() throws NoMoreNamesAvailableException, PropertyVetoException;
+
+    /**
+     * Returns the UI adapter of the rule. The adapter must implement the
+     * {@link IMemberRenamingRuleAdapter} interface.
+     * 
+     * @return UI adapter of the rule.
+     */
     public IMemberRenamingRuleAdapter getAdapter();
 }
