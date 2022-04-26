2a985b723e18a83efcdf3b633413b8971dd6383f
tools400 <thomas.raddatz@tools400.de>
2022-04-26 15:24:40 +0000

    Deleted incorrect tag of 4.2.3.r.

--- tags/v_004.002.003_DELETE/tags/v_003.006.001/tags/Journal Explorer (Isaac Ramirezh)/org.bac.gati.tools.journalexplorer.ui/src/org/bac/gati/tools/journalexplorer/internals/QualifiedName.java
+++
@@ -1,8 +0,0 @@
-package org.bac.gati.tools.journalexplorer.internals;
-
-public class QualifiedName {
-
-	public static String getName(String library, String objectName) {
-		return library.trim() + '/' + objectName.trim();
-	}
-}
