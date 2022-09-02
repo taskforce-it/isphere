7e6f95f5c53af8811604c2ed87714b13a3aedfa3
tools400 <thomas.raddatz@tools400.de>
2022-09-02 13:09:35 +0000

    Changed export filter dialog to not show a confirmation message if a filter does not exist.

--- eclipse/iSphere Core Plugin/src/biz/isphere/core/internal/FilterDialog.java
+++ eclipse/iSphere Core Plugin/src/biz/isphere/core/internal/FilterDialog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012-2014 iSphere Project Owners
+ * Copyright (c) 2012-2022 iSphere Project Owners
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
@@ -214,7 +214,7 @@ public class FilterDialog extends XDialog {
 
         cbFilter.setText(cbFilter.getText().trim());
 
-        if (getFilterUpdateType() == FilterUpdateType.REPLACE) {
+        if (filterExists() && getFilterUpdateType() == FilterUpdateType.REPLACE) {
             if (!confirmReplace(cbFilter.getText())) {
                 return;
             }
@@ -233,6 +233,10 @@ public class FilterDialog extends XDialog {
 
         super.okPressed();
 
+    }
+
+    private boolean filterExists() {
+        return gpUpdateType.isEnabled();
     }
 
     private boolean confirmReplace(String filterName) {
