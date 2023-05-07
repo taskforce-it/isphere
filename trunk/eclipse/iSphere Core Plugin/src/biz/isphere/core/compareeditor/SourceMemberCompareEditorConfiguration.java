5f1bbc9c837ddf967caa75fcbbdcb17735a826bc
tools400 <thomas.raddatz@tools400.de>
2023-05-07 11:03:26 +0000

    Removed unnecessary code line.

--- trunk/eclipse/iSphere Core Plugin/src/biz/isphere/core/compareeditor/SourceMemberCompareEditorConfiguration.java
+++ trunk/eclipse/iSphere Core Plugin/src/biz/isphere/core/compareeditor/SourceMemberCompareEditorConfiguration.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012-2021 iSphere Project Team
+ * Copyright (c) 2012-2023 iSphere Project Team
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,14 +8,11 @@
 
 package biz.isphere.core.compareeditor;
 
-import biz.isphere.core.preferences.Preferences;
 
 public class SourceMemberCompareEditorConfiguration extends CompareEditorConfiguration {
 
     public SourceMemberCompareEditorConfiguration() {
         super();
-
-        setProperty(IGNORE_WHITESPACE, Preferences.getInstance().isSourceMemberCompareIgnoreWhiteSpaces());
     }
 
 }
