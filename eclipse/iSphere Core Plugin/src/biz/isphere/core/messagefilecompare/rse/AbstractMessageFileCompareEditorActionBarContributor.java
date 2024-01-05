ff80014f5fdb2b7c140f0e0a0c883c95a1a01532
Thomas Raddatz <thomas.raddatz@tools400.de>
2024-01-05 21:19:17 +0100

    Check type before casting.

--- eclipse/iSphere Core Plugin/src/biz/isphere/core/messagefilecompare/rse/AbstractMessageFileCompareEditorActionBarContributor.java
+++ eclipse/iSphere Core Plugin/src/biz/isphere/core/messagefilecompare/rse/AbstractMessageFileCompareEditorActionBarContributor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012-2014 iSphere Project Owners
+ * Copyright (c) 2012-2024 iSphere Project Owners
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
@@ -35,10 +35,11 @@ public abstract class AbstractMessageFileCompareEditorActionBarContributor exten
             return;
         }
 
-        activeEditorPart = (AbstractMessageFileCompareEditor)editorPart;
-
-        activeEditorPart.setStatusLine(statusLineContribution.getStatusLine());
-        activeEditorPart.updateActionsStatusAndStatusLine();
+        if (editorPart instanceof AbstractMessageFileCompareEditor) {
+            activeEditorPart = (AbstractMessageFileCompareEditor)editorPart;
+            activeEditorPart.setStatusLine(statusLineContribution.getStatusLine());
+            activeEditorPart.updateActionsStatusAndStatusLine();
+        }
     }
 
     public abstract String getStatusLineId();
